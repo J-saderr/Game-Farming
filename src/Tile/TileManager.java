59c52ac9be8f78a94a81217daeab633370bb1ffa
Sleepapp <vontthao@gmail.com>
2023-12-14 09:22:32 +0700

    Chỉnh lại class vì bị rối

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -20,10 +20,22 @@ public class TileManager {
 
     public void getTileManager() {
 
-        String filepath00 = "res/tile/00.png";
-        String filepath05 = "res/tile/05.png";
-        try (InputStream inputStream00 = new FileInputStream(new File(filepath00));
-             InputStream inputStream05 = new FileInputStream(new File(filepath05))){
+        try (InputStream inputStream00 = new FileInputStream(new File("res/tile/00.png"));
+             InputStream inputStream05 = new FileInputStream(new File("res/tile/05.png"));
+             InputStream inputStreamup = new FileInputStream(new File("res/tile/up.png"));
+             InputStream inputStreamleftup = new FileInputStream(new File("res/tile/leftup.png"));
+             InputStream inputStreamleft = new FileInputStream(new File("res/tile/left.png"));
+             InputStream inputStreamleftbottom = new FileInputStream(new File("res/tile/leftbottom.png"));
+             InputStream inputStreambottom = new FileInputStream(new File("res/tile/bottom.png"));
+             InputStream inputStreamrightbottom = new FileInputStream(new File("res/tile/rightbottom.png"));
+             InputStream inputStreamright = new FileInputStream(new File("res/tile/right.png"));
+             InputStream inputStreamrightup = new FileInputStream(new File("res/tile/rightup.png"));
+             InputStream inputStreamroad1up = new FileInputStream(new File("res/tile/road1up.png"));
+             InputStream inputStreamroad1down = new FileInputStream(new File("res/tile/road1down.png"));
+             InputStream inputStreamroad2up = new FileInputStream(new File("res/tile/road2up.png"));
+             InputStream inputStreamroad2down = new FileInputStream(new File("res/tile/road2down.png"));
+             InputStream inputStreamroad3up = new FileInputStream(new File("res/tile/road3up.png"));
+             InputStream inputStreamroad3down = new FileInputStream(new File("res/tile/road3down.png"))) {
 
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(inputStream00);
@@ -31,6 +43,47 @@ public class TileManager {
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(inputStream05);
 
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(inputStreamup);
+
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(inputStreamleftup);
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(inputStreamleft);
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(inputStreamleftbottom);
+
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(inputStreambottom);
+
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(inputStreamrightbottom);
+
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(inputStreamright);
+
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(inputStreamrightup);
+
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(inputStreamroad1up);
+
+            tile[11] = new Tile();
+            tile[11].image = ImageIO.read(inputStreamroad1down);
+
+            tile[12] = new Tile();
+            tile[12].image = ImageIO.read(inputStreamroad2up);
+
+            tile[13] = new Tile();
+            tile[13].image = ImageIO.read(inputStreamroad2down);
+
+            tile[14] = new Tile();
+            tile[14].image = ImageIO.read(inputStreamroad3up);
+
+            tile[15] = new Tile();
+            tile[15].image = ImageIO.read(inputStreamroad3down);
 
         } catch (IOException e) {
             e.printStackTrace();
